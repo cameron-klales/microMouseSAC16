399dbea48b82c4eda0081d9f002a62a966750509
Brent Tillman <brnttil@gmail.com>
2016-04-03 19:22:12 -0400

    Update mainfunction.go

--- mainfunction.go
+++ mainfunction.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"floodfill"
+	"Flooding_Alg_Golang"
 )
 
 func main() {
-	maze := floodFill.Maze{}
+	maze := Flooding_Alg_Golang.Maze{}
 	maze = maze.Constructor(5, 5,2,2)
-	floodFill.Solve(maze);
+	Flooding_Alg_Golang.Solve(maze);
 	//  fmt.Print(maze.ROWS);
-	floodFill.AddWall(maze,1)
-	floodFill.AddWallByLocation(maze,3,3,0)
+	Flooding_Alg_Golang.AddWall(maze,1)
+	Flooding_Alg_Golang.AddWallByLocation(maze,3,3,0)
 	maze.Print()
 }
