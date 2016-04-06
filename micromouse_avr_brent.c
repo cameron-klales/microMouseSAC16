557761f1474cb8c7ef8207629f8743dfa62f93c6
Brent Tillman <brnttil@gmail.com>
2016-04-06 09:26:58 -0400

    Update micromouse_avr_brent.c

--- micromouse_avr_brent.c
+++ micromouse_avr_brent.c
@@ -114,11 +114,9 @@ int main()
 		}
 		last_proximity = proximity; // remember last proximity for derivative
 	}
-	// This part of the code is never reached. A robot should
-	// never reach the end of its program, or unpredictable behavior
-	// will result as random code starts getting executed. If you
-	// really want to stop all actions at some point, set your motors
-	// to 0,0 and run the following command to loop forever:
-	//
-	// while(1);
+	// This part of the code is never reached. 
+	while(1)
+	{
+		set_motors(0,0)
+	}
 }
